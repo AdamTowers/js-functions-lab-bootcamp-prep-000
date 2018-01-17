4737911d1197b010df001c645407b44cec1d7acd
Adam Towers <hello@adamtowers.design>
2018-01-17 14:07:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 // write your code below!
 function happyHolidays() {
-  return "Happy Holidays!"
+  return `Happy Holidays!`
 }
 
 function happyHolidaysTo(name) {
-  return "Happy Holidays To ${name}"
+  return `Happy Holidays To ${name}`
 }
 
 function happyHolidayTo(holiday, name) {
-  return "Happy ${holiday}, ${name}"
+  return `Happy ${holiday}, ${name}`
 }
 
-function 
+function holidayCountdown(days, holiday) {
+  return `It's ${days} until ${holiday}`
+}
